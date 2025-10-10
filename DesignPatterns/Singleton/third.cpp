e667aaf7703973368d4a6aad717f294380df1309
ravirj0601 <ravirj0601@gmail.com>
2025-10-10 22:36:22 +0530

    Fix: SimpleLogger constructor and destructor declarations
    - Declared private constructor and destructor in SimpleLogger class to resolve redefinition errors.
    - Ensured out-of-line definitions match declarations.

--- DesignPatterns/Singleton/third.cpp
+++ DesignPatterns/Singleton/third.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
 using namespace std;
@@ -8,16 +9,16 @@ class SimpleLogger{
     private:
         ofstream out_;
         string fileName_;
+        mutex mtx;
 
-        SimpleLogger(){}
-        ~SimpleLogger(){}
+        SimpleLogger();
+        ~SimpleLogger();
 
         SimpleLogger(const SimpleLogger&) = delete;
         SimpleLogger& operator=(const SimpleLogger&) = delete;
 
     public:
         static SimpleLogger& instance();
-
         void log(const string& msg);
         void flush();
 };
@@ -37,10 +38,30 @@ SimpleLogger::SimpleLogger() : fileName_("app.log"){
 void SimpleLogger::log(const string& msg){
     std::ostringstream line;
     line << msg << "\n";
+    {
+        lock_guard<mutex> lk(mtx);
+        if (out_.is_open()) {
+            out_ << line.str();
+        }else {
+            std::cerr << line.str();
+        }
+    }
+}
+
+void SimpleLogger::flush(){
+    lock_guard<mutex> lk(mtx);
+    if (out_.is_open()) out_.flush();
+}
+
+SimpleLogger::~SimpleLogger(){
+    if (out_.is_open()) out_.close();
 }
 
 int main() {
-    
+    SimpleLogger::instance().log("Program Started: ");
+    SimpleLogger::instance().log("Doing Work: ");
+    SimpleLogger::instance().log("End: ");
+    SimpleLogger::instance().flush();
     return 0;
 }
 
